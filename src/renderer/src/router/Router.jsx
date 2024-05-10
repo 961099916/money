77600f5c2be43bf8ec0945077e2d439baba79357
zhangjiahao <961099916@qq.com>
2024-05-10 17:52:47 +0800

    init

---
+++ src/renderer/src/router/Router.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import './App.css'
+import Index from './pages/Index'
+import LimitUp from './pages/limit-up/LimitUp'
+import Emotional from './pages/emotional-cycle/Emotional'
+import PlateRotation from './pages/plate-rotation/PlateRotation'
+import WinnersList from './pages/winners-list/WinnersList'
+import DragonTotem from './pages/dragon-totem/DragonTotem'
+import TimeSharing from './pages/time-sharing/TimeSharing'
+
+export default createBrowserRouter([
+  {
+    path: '/',
+    element: <Index />,
+    children: [
+      {
+        path: '/limit-up',
+        element: <LimitUp />
+      },
+      {
+        path: '/',
+        element: <LimitUp />
+      },
+      {
+        path: '/emotional',
+        element: <Emotional />
+      },
+      {
+        path: '/plate-rotation',
+        element: <PlateRotation />
+      },
+      {
+        path: '/dragon-totem',
+        element: <DragonTotem />
+      },
+      {
+        path: '/winners-list',
+        element: <WinnersList />
+      },
+      {
+        path: '/time-sharing',
+        element: <TimeSharing />
+      }
+    ]
+  }
+])
